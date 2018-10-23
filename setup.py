367f3361560c4d479eded33628af268b95a49b41
Alisson Lauffer <alissonvitortc@gmail.com>
2018-10-23 13:25:04 -0300

    Add Python 3.7

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     keywords='telegram bot api python wrapper',
