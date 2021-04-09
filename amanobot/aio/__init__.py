cf312878d15912bc4ac906b269bda77f7a1e46a8
Alisson Lauffer <alissonvitortc@gmail.com>
2021-04-09 12:24:34 -0300

    Add missed update types

--- amanobot/aio/__init__.py
+++ amanobot/aio/__init__.py
@@ -961,11 +961,15 @@ class Bot(_BotBase):
                                                'edited_message',
                                                'channel_post',
                                                'edited_channel_post',
-                                               'callback_query',
                                                'inline_query',
                                                'chosen_inline_result',
+                                               'callback_query',
                                                'shipping_query',
-                                               'pre_checkout_query'])
+                                               'pre_checkout_query',
+                                               'poll',
+                                               'poll_answer',
+                                               'my_chat_member',
+                                               'chat_member'])
 
                 callback(update[key])
             except:
